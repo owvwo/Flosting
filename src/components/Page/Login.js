561048b2a2b70fc87ac29855ac014fe284ee3eb4
최승훈 <chhoon97@naver.com>
2021-06-29 18:38:29 +0900

    test 5

--- src/components/Page/Login.js
+++ src/components/Page/Login.js
@@ -39,24 +39,24 @@ const Button = styled.button`
 const Input = styled.input.attrs(props => ({
     type: "text",
   }))`
-    line-height: 3rem;
-    padding: 5px;
-    margin: 5px;
-    height: 3rem;
-    width: 300px;
-    font-size: 1rem;
-    border: 2px solid #E0BCC1;
-    border-radius: 5px;
-  
-    /* here we use the dynamically computed prop */
-    margin: ${props => props.size};
-    padding: ${props => props.size};
+  line-height: 3rem;
+  padding: 5px;
+  margin: 5px;
+  height: 3rem;
+  width: 300px;
+  font-size: 1rem;
+  border: 2px solid #E0BCC1;
+  border-radius: 5px;
 `;
 
 const Loginbar = () => {
     return (
         <Container>
-
+            <Input placeholder="학번" />
+            <Input placeholder="비밀번호" />
+            <Button register>
+                로그인
+            </Button>
         </Container>
     )
 }
