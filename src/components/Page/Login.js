a4542464e6c319b01ba5313440b5e58801e1e6b9
최승훈 <chhoon97@naver.com>
2021-06-29 17:23:18 +0900

    Apply test

--- src/components/Page/Login.js
+++ src/components/Page/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () =>{
     return(
         <div>
-            <h1>로그인</h1>
+            <h1>로그인d</h1>
             <h2>정우 치팅 ㄱ</h2>
         </div>
     );
